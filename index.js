6d662f53048bdd9915d1e63ab103466218cc6497
nandabi <valerie.nandabi@student.moringaschool.com>
2023-12-21 09:50:17 +0300

    commit message

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers= function(drivers){
+    return drivers.slice(0,2)
+}
+const returnLastTwoDrivers=function(drivers) {
+    return drivers.slice(-2,driverslength)
+}
+const selectingDrivers= [returnFirstTwoDrivers,returnLastTwoDrivers]    
+ const createFareMultiplier = function(mult){
+    return function(fare){
+        return fare.mult
+    }
+ }
+ function fareDoubler(fare){
+    return createFareMultiplier(2)(fare)
+ }
+ function fareTripler(fare){
+    return createFareMultiplier(3)(fare)
+ }  
+ function selectDifferentDrivers(drivers,func){
+    return func(drivers)
+ }
